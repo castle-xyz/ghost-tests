3af88525c10b1032984159d61c20580acbb65307
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2018-12-10 16:00:37 -0800

    add `love.textinput` test

---
+++ textinput/main.lua
@@ -0,0 +1,26 @@
+local utf8 = require 'utf8'
+
+local text
+
+function love.load()
+    text = 'Start typing! -- '
+    love.keyboard.setKeyRepeat(true)
+end
+
+function love.textinput(t)
+    text = text .. t
+end
+
+function love.keypressed(key)
+    if key == "backspace" then
+        local byteoffset = utf8.offset(text, -1)
+        if byteoffset then
+            text = string.sub(text, 1, byteoffset - 1)
+        end
+    end
+end
+
+function love.draw()
+    love.graphics.printf(text, 20, 20, love.graphics.getWidth() - 20)
+end
+
