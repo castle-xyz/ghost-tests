11ffb19bb56711a4f7fd7f27482690201f4d770a
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-01-16 15:01:46 -0800

    desktop: enable + test `castle.isLoggedIn` tracking state after load

--- login/main.lua
+++ login/main.lua
@@ -7,4 +7,6 @@ end
 function love.draw()
     love.graphics.print('`castle.isLoggedIn` in `love.load()`: ' .. tostring(isLoggedInLoad))
     love.graphics.print('\n`castle.isLoggedIn` in `love.draw()`: ' .. tostring(castle.isLoggedIn))
+
+    love.graphics.print('\n\nmake sure to test changing login / logout state while game is running')
 end
