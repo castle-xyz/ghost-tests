bdc5fbb5f26117b5d29eb8c71c43493fdba710c6
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-27 22:49:04 -0700

    test `castle.user.` apis

---
+++ user/main.lua
@@ -0,0 +1,26 @@
+local photo
+
+function love.draw()
+    if castle.user.isLoggedIn == true then
+        local me = castle.user.me()
+        if me then
+            love.graphics.print('username: ' .. me.username, 20, 20)
+            love.graphics.print('\nname: ' .. me.name, 20, 20)
+            love.graphics.print('\n\nphotoUrl: ' .. me.photoUrl, 20, 20)
+
+            if photo then
+                love.graphics.draw(photo, 20, 88)
+            else
+                network.async(function()
+                    photo = love.graphics.newImage(me.photoUrl)
+                end)
+            end
+        end
+    end
+    if castle.user.isLoggedIn == false then
+        love.graphics.print('not logged in', 20, 20)
+    end
+    if castle.user.isLoggedIn == nil then
+        print('nil!')
+    end
+end
