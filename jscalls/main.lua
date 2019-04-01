7a3821a4a014e0d2dd95cd8f8acd3c0e8f1a5813
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-03-31 23:40:27 -0700

    test `jsCalls`

---
+++ jscalls/main.lua
@@ -0,0 +1,18 @@
+local jsCalls = require 'jsCalls'
+
+function love.draw()
+    love.graphics.print('press any key other than SPACE to trigger a JS call for that key', 20, 20)
+    love.graphics.print('\npress SPACE to trigger an unknown JS call', 20, 20)
+end
+
+function love.keypressed(key)
+    if key ~= 'space' then
+        network.async(function()
+            print(key .. ': ' .. jsCalls.sayHello { name = key })
+        end)
+    else
+        network.async(function()
+            print(key .. ': ' .. jsCalls.noSuchMethod { name = key })
+        end)
+    end
+end
