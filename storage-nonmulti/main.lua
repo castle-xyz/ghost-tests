e8b9ba5874cdd98ea0494a5390eda219cca762e9
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-04-01 20:30:14 -0700

    test non-multiplayer storage get and set

--- storage-nonmulti/main.lua
+++ storage-nonmulti/main.lua
@@ -1,3 +1,37 @@
+local selectedKeyName = 'key1'
+
 function love.draw()
-    love.graphics.print('hello, world!', 20, 20)
+    love.graphics.print([[
+    press G to get the current value
+
+    press Q, W, or E to set the value to 'q', 'w' or 'e' respectively
+
+    press D to set the value to `nil`
+
+    press 1, 2 or 3 to change the selected key to 'key1', 'key2' or 'key3' respectively
+    the currently selected key is ']] .. selectedKeyName .. [[''
+    ]], 20, 20)
+end
+
+function love.keypressed(key)
+    if key == 'g' then
+        network.async(function()
+            local value = castle.storage.getGlobal(selectedKeyName) 
+            local desc = (value == nil) and '`nil`' or "'" .. value .. "'"
+            print("got '" .. selectedKeyName .. "' as " .. desc)
+        end)
+    end
+
+    if key == 'q' or key == 'w' or key == 'e' then
+        network.async(function()
+            local value = (key ~= 'd') and key or nil
+            castle.storage.setGlobal(selectedKeyName, key)
+            local desc = (value == nil) and '`nil`' or "'" .. value .. "'"
+            print("set '" .. selectedKeyName .. "' to " .. desc)
+        end)
+    end
+
+    if key == '1' or key == '2' or key == '3' then
+        selectedKeyName = 'key' .. key
+    end
 end
