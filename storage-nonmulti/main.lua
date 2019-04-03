441f33d92d86f15aece8f9dbae3b3073a171eccc
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-04-03 14:53:38 -0700

    attempt testing user storage

--- storage-nonmulti/main.lua
+++ storage-nonmulti/main.lua
@@ -1,4 +1,5 @@
 local selectedKeyName = 'key1'
+local mode = 'GLOBAL'
 
 function love.draw()
     love.graphics.print([[
@@ -10,13 +11,17 @@ function love.draw()
 
     press 1, 2 or 3 to change the selected key to 'key1', 'key2' or 'key3' respectively
     the currently selected key is ']] .. selectedKeyName .. [['
+
+    press TAB to toggle the storage mode
+    the currently selected mode is ]] .. mode .. [[
     ]], 20, 20)
 end
 
 function love.keypressed(key)
     if key == 'g' then
         network.async(function()
-            local value = castle.storage.getGlobal { key = selectedKeyName } 
+            local funcName = mode == 'GLOBAL' and 'getGlobal' or 'get'
+            local value = castle.storage[funcName](selectedKeyName)
             local desc = (value == nil) and '`nil`' or "'" .. value .. "'"
             print("got '" .. selectedKeyName .. "' as " .. desc)
         end)
@@ -24,8 +29,9 @@ function love.keypressed(key)
 
     if key == 'q' or key == 'w' or key == 'e' or key == 'd' then
         network.async(function()
+            local funcName = mode == 'GLOBAL' and 'setGlobal' or 'set'
             local value = (key ~= 'd') and key or nil
-            castle.storage.setGlobal { key = selectedKeyName, value = value }
+            castle.storage[funcName](selectedKeyName, value)
             local desc = (value == nil) and '`nil`' or "'" .. value .. "'"
             print("set '" .. selectedKeyName .. "' to " .. desc)
         end)
