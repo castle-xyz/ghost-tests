0f0f3c1f63447c1e546db7a027be10db9a348dfd
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-04-20 15:10:55 -0400

    maybe test `caslte.postopened`

--- post/main.lua
+++ post/main.lua
@@ -1,16 +1,28 @@
+local text = ''
+
+function castle.postopened(data)
+    text = data.text
+end
+
 function love.draw()
     love.graphics.print([[
-    press P to make a post
+    press ENTER to make a post
+
+    the text so far is: []] .. text .. [[]
     ]], 20, 20)
 end
 
 function love.keypressed(key)
-    if key == 'p' then
+    if key == 'enter' then
         network.async(function()
             castle.post.create {
                 message = 'hello, world!',
-                data = { a = 1, b = 2 },
+                data = { text = text },
             }
         end)
     end
+end
+
+function love.textinput(t)
+    text = text .. t
 end
