c56062ac3ecea575ce3494af2a865658e9ff8775
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-05-01 16:05:26 -0700

    fix `castle.game.` tests

--- gameload2/main.lua
+++ gameload2/main.lua
@@ -1,9 +1,18 @@
+local initialParams = castle.game.getInitialParams()
+local msg = initialParams and initialParams.msg or '<no msg>'
+
 function love.draw()
-    love.graphics.print('game 2\npress L to load other game', 20, 20);
+    love.graphics.print('game 2\npress L to load other game\n\nmsg we got was: ' .. msg, 20, 20);
 end
 
-function love.keypressed(k)
+function love.keypressed(key)
     if key == 'l' then
-        castle.game.load('https://raw.githubusercontent.com/castle-games/ghost-tests/master/gameload1/project.castle', { msg = 'from game 2' })
+        network.async(function()
+            castle.game.load(
+                'https://raw.githubusercontent.com/castle-games/ghost-tests/master/gameload1/project.castle', {
+                    msg = 'from game 2',
+                }
+            )
+        end)
     end
 end
