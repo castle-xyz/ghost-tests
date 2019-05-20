39491977c3058b0d05ebba516d11222352f12d41
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-05-19 18:28:50 -0700

    nicer circle demo

--- ui/circles.lua
+++ ui/circles.lua
@@ -4,15 +4,17 @@ local circles = {}
 
 function castle.uiupdate()
     ui.box({ gap = 'small' }, function()
+        -- Button for adding circles
         if ui.button('add circle') then
             table.insert(circles, {
                 x = love.graphics.getWidth() * math.random(),
                 y = love.graphics.getHeight() * math.random(),
-                radius = 5 + 80 * math.random(),
+                radius = 30 + 100 * math.random(),
                 color = ({ 'red', 'blue', 'green' })[math.floor(3 * math.random()) + 1]
             })
         end
 
+        -- Section per circle
         for i, circle in ipairs(circles) do
             ui.section('circle ' .. i, function()
                 circle.color = ui.radioButtonGroup('color', circle.color, { 'red', 'blue', 'green' })
@@ -24,16 +26,19 @@ function castle.uiupdate()
                 circle.y = ui.rangeInput('y', circle.y, 0, love.graphics.getHeight(), 1)
 
                 ui.text('radius')
-                circle.radius = ui.rangeInput('radius', circle.radius, 5, 85, 1)
+                circle.radius = ui.rangeInput('radius', circle.radius, 30, 105, 1)
             end)
         end
     end)
 end
 
+local labelFont = love.graphics.newFont(28)
+
 function love.draw()
     love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
 
     for i, circle in ipairs(circles) do
+        -- Draw the circle
         if circle.color == 'red' then
             love.graphics.setColor(1, 0, 0)
         elseif circle.color == 'green' then
@@ -42,5 +47,12 @@ function love.draw()
             love.graphics.setColor(0, 0, 1)
         end
         love.graphics.circle('fill', circle.x, circle.y, circle.radius)
+
+        -- Draw a label over the circles to show which one is which
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.setFont(labelFont)
+        local label = tostring(i)
+        local tw, th = labelFont:getWidth(i), labelFont:getHeight()
+        love.graphics.print(label, circle.x - 0.5 * tw, circle.y - 0.5 * th)
     end
 end
