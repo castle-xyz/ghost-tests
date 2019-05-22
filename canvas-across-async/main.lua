bfbe0feb02930a6f084482f1e81eba3ec5877b7e
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-05-22 14:43:42 -0700

    test with `network.{pause,resume}`

--- canvas-across-async/main.lua
+++ canvas-across-async/main.lua
@@ -1,13 +1,24 @@
 function love.load()
   print('start')
 
---   local canvas = love.graphics.newCanvas(32, 32)
+  local canvas = love.graphics.newCanvas(32, 32)
   
---   love.graphics.setCanvas(canvas)
+  love.graphics.setCanvas(canvas)
   
   local image = love.graphics.newImage('test.png')
   
---   love.graphics.setCanvas()
+  love.graphics.setCanvas()
   
   print('finish')
+end
+
+local savedCanvas
+
+function network.pause()
+  savedCanvas = love.graphics.getCanvas()
+  love.graphics.setCanvas()
+end
+
+function network.resume()
+  love.graphics.setCanvas(savedCanvas)
 end
