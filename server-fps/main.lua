79ac004c39775a6809ab8ba425e81234f3a6cd2c
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-07-15 16:32:02 -0700

    disable own throttling in server throttle test

--- server-fps/main.lua
+++ server-fps/main.lua
@@ -1,4 +1,4 @@
-local OWN_THROTTLING = true
+local OWN_THROTTLING = false
 
 
 local cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/34cc93e9e35231de2ed37933d82eb7c74edfffde/cs.lua'
