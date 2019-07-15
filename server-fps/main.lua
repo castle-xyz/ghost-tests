87497129f351c16ab4c7ac8c06659f499cf31a1b
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-07-15 13:43:05 -0700

    update server fps test for proper server initialization

--- server-fps/main.lua
+++ server-fps/main.lua
@@ -1,11 +1,18 @@
+local cs = require 'https://raw.githubusercontent.com/castle-games/share.lua/34cc93e9e35231de2ed37933d82eb7c74edfffde/cs.lua'
+
 local isRemoteServer = castle.system.isRemoteServer()
 
 local lastReportTime = love.timer.getTime()
 
-function love.update(dt)
+local function update(dt)
     local currTime = love.timer.getTime()
     if currTime - lastReportTime >= 1.5 then
         print((isRemoteServer and 'server: ' or 'client: ') .. love.timer.getFPS())
         lastReportTime = currTime
     end
 end
+
+local server, client = cs.server, cs.client
+
+cs.server.update = update
+cs.client.update = update
