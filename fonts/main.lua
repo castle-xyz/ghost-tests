9a201618d81ff090957b6caef1ae27518548739a
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2019-11-22 15:47:53 -0800

    more work on font tests

--- fonts/main.lua
+++ fonts/main.lua
@@ -1,19 +1,23 @@
-local unscii16, unscii48
+local unscii16, unscii48, default14
 
-function love.resize()
-    unscii16 = love.graphics.newFont('unscii-16.ttf', 16, 'mono')
-    unscii16:setFilter('nearest', 'nearest')
+unscii16 = love.graphics.newFont('unscii-16.ttf', 16, 'mono')
+unscii16:setFilter('nearest', 'nearest')
 
-    unscii48 = love.graphics.newFont('unscii-16.ttf', 48, 'mono')
-    unscii48:setFilter('nearest', 'nearest')
-end
+unscii48 = love.graphics.newFont('unscii-16.ttf', 48, 'mono')
+unscii48:setFilter('nearest', 'nearest')
 
-love.resize()
+print('dpiscale', unscii48:getDPIScale())
+
+default14 = love.graphics.newFont(14)
 
 function love.draw()
     love.graphics.setFont(unscii16)
-    love.graphics.print('hello, world! 123456', 20, 20)
+    love.graphics.print('fps: ' .. love.timer.getFPS(), 20, 20)
+    love.graphics.print('hello, world! 123456', 20, 100)
 
     love.graphics.setFont(unscii48)
     love.graphics.print('hello, world! 123456', 20, 200)
+
+    love.graphics.setFont(default14)
+    love.graphics.print('hello, world! 123456', 20, 400)
 end
